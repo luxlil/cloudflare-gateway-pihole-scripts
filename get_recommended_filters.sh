88156bb9af8f74dad558f3ea996cf80590ff4198
Lil <141233170+luxlil@users.noreply.github.com>
2024-09-23 14:34:41 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -11,15 +11,22 @@ urls=(
     https://abpvn.com/filter/abpvn-bOio7G.txt
     https://abpvn.com/vip/admin.txt
     https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
-    https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
-    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt
+    https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/mobile.txt
+    https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/tracking_servers.txt
+    https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/tracking_servers_firstparty.txt
     https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/issues.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/mac.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/extensions/threat/filter.txt
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers.txt
+    https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+    https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_thirdparty.txt
+    https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_thirdparty_international.txt
+    https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_trackingservers.txt
+    https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_trackingservers_international.txt
+    https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt
+    https://raw.githubusercontent.com/List-KR/List-KR/master/filters-share/1st_domains.txt
+    https://raw.githubusercontent.com/List-KR/List-KR/master/filters-share/3rd_domains.txt
     # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
     # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
