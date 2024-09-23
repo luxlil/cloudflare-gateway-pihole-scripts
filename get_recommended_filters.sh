23771dc4f902c56c5eb3fec1ffc2b54959b4c0e7
Lil <141233170+luxlil@users.noreply.github.com>
2024-09-23 11:37:18 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,4 +1,3 @@
-  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
-  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
-  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+#!/bin/bash
+
+node download_lists.js blocklist
