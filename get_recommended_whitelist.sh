ed10ad1a48ba90bc6ab886c4b8116e56ec8b6f21
Lil <141233170+luxlil@users.noreply.github.com>
2024-09-23 11:12:38 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-
-node download_lists.js allowlist
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
+https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
