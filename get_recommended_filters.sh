a4585f721ee54666aa5840b88850b1053edbb95a
Lil <141233170+luxlil@users.noreply.github.com>
2024-09-23 11:39:19 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,19 @@
-#!/bin/bash
+# Use the provided lists or add you own
+# https://oisd.nl/includedlists/whitelists
+# by creating an empty whitelist.csv file
+touch whitelist.csv
 
-node download_lists.js blocklist
+# declare an array of urls
+urls=(
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
+    # Commented out because it looks suspicious
+    # https://www.aadvantageeshopping.com/adBlockWhitelist.php
+    https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/issues.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/mac.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
+    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
+    # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
+    # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
