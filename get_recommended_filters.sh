56fee33c54630b156503dbe8ea1166566050990a
Lil <141233170+luxlil@users.noreply.github.com>
2024-09-23 11:12:02 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-
-node download_lists.js blocklist
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
